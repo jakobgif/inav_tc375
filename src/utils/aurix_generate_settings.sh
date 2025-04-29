e1df1fd584f13c2f78285a6ae1a763548a8c2ab7
Jakob <jakob.frenzel@hotmail.com>
2025-04-29 18:28:43 +0200

    added some echo outputs

--- src/utils/aurix_generate_settings.sh
+++ src/utils/aurix_generate_settings.sh
@@ -4,6 +4,7 @@ BUILD_FOLDER=$PWD
 #echo $BUILD_FOLDER
 
 #convert inlcude path from aurix to unix path
+echo creating include paths
 sed -E -e 's#-I"C:([^"]+)"#-I"/mnt/c\1"#g' -e 's#\\#/#g' "AURIX_GCC_Compiler-Include_paths__-I_.opt" > includes.opt
 INCLUDES=$(<includes.opt)
 #echo $INCLUDES
@@ -13,13 +14,17 @@ cd "$(dirname "$0")"
 
 #compile definitions
 DEFS="-DTC375"
+#IMPORTANT: this can vary by machine
 export CFLAGS="$INCLUDES -I/mnt/c/Infineon/AURIX-Studio-1.10.2/tools/Compilers/tricore-gcc11/tricore-elf/include $DEFS"
 export SETTINGS_CXX="g++"
 
 SETTINGS_FILE=../main/fc/settings.yaml
 OUTPUT_DIR=$BUILD_FOLDER #../main/target
 
+echo running settings.rb
 ruby ./settings.rb \
     "../" \
     "$SETTINGS_FILE" \
     -o "$OUTPUT_DIR"
+
+echo done
