7e692c47b2fee37b9afbef674f455c8370860a1d
Jakob <jakob.frenzel@hotmail.com>
2025-06-02 20:09:18 +0200

    fixed copy paste error in version.c

--- src/main/build/version.c
+++ src/main/build/version.c
@@ -24,7 +24,7 @@ const char * const shortGitRevision = GIT_TAG_INAV;
 #ifdef BUILD_CONFIG_RELEASE
 const char * const buildType = STR_HELPER(BUILD_CONFIG_RELEASE);
 #elif BUILD_CONFIG_DEBUG
-const char * const buildType = STR_HELPER(BUILD_CONFIG_RELEASE);
+const char * const buildType = STR_HELPER(BUILD_CONFIG_DEBUG);
 #endif
 #else
 const char * const targetName = __TARGET__;
