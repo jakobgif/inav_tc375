b0a275f612a3f8f40a96fc733f61357075f44caf
Jakob <jakob.frenzel@hotmail.com>
2025-08-22 14:28:46 +0200

    moved some code sections, implemented DMA PWM generation

--- src/main/drivers/timer_impl_tc375.c
+++ src/main/drivers/timer_impl_tc375.c
@@ -38,7 +38,7 @@ void impl_timerInitContext(TCH_t * tch){
 
 volatile timCCR_t * impl_timerCCR(TCH_t * tch){
     Ifx_GTM_ATOM_CH *atomCh = IfxGtm_Atom_Ch_getChannelPointer(tch->timHw->tim->atom, tch->timHw->triggerOut->channel);
-    return (volatile timCCR_t *)&(atomCh->CM1.U);
+    return (volatile timCCR_t *)&(atomCh->SR1.U);
 }
 
 void impl_timerNVICConfigure(TCH_t * tch, int irqPriority){
@@ -52,47 +52,57 @@ void impl_timerConfigBase(TCH_t * tch, uint16_t period, uint32_t hz){
                 
     IfxGtm_Atom_Timer_initConfig(atomConfig, &MODULE_GTM); // Initialize default parameters
 
-    switch(tch->timHw->triggerOut->atom)
+    atomConfig->atom = tch->timHw->triggerOut->atom;
+    atomConfig->timerChannel = tch->timHw->triggerOut->channel;
+
+    IfxGtm_Atom_Timer_init(tch->timHw->tim, atomConfig); //init ATOM
+
+    //init has to be done before because pointer in timHw->tim not set yet
+    Ifx_GTM_ATOM *atom = tch->timHw->tim->atom;
+    IfxGtm_Atom_Ch channel = tch->timHw->tim->timerChannel;
+
+    switch(tch->timHw->tim->atomIndex)
     {
         case IfxGtm_Atom_0:
             atomConfig->clock = IfxGtm_Cmu_Clk_0; // Select the CMU clock
             IfxGtm_Cmu_setClkFrequency(&MODULE_GTM, IfxGtm_Cmu_Clk_0, (float32)hz); // Set the clock frequency
             IfxGtm_Cmu_enableClocks(&MODULE_GTM, IFXGTM_CMU_CLKEN_CLK0); // Enable the CMU clock 0
+            IfxGtm_Atom_Ch_setClockSource(atom, channel, IfxGtm_Cmu_Clk_0);
             break;
         case IfxGtm_Atom_1:
             atomConfig->clock = IfxGtm_Cmu_Clk_1;
             IfxGtm_Cmu_setClkFrequency(&MODULE_GTM, IfxGtm_Cmu_Clk_1, (float32)hz);
             IfxGtm_Cmu_enableClocks(&MODULE_GTM, IFXGTM_CMU_CLKEN_CLK1);
+            IfxGtm_Atom_Ch_setClockSource(atom, channel, IfxGtm_Cmu_Clk_1);
             break;
         case IfxGtm_Atom_2:
             atomConfig->clock = IfxGtm_Cmu_Clk_2;
             IfxGtm_Cmu_setClkFrequency(&MODULE_GTM, IfxGtm_Cmu_Clk_2, (float32)hz);
             IfxGtm_Cmu_enableClocks(&MODULE_GTM, IFXGTM_CMU_CLKEN_CLK2);
+            IfxGtm_Atom_Ch_setClockSource(atom, channel, IfxGtm_Cmu_Clk_2);
             break;
         case IfxGtm_Atom_3:
             atomConfig->clock = IfxGtm_Cmu_Clk_3;
             IfxGtm_Cmu_setClkFrequency(&MODULE_GTM, IfxGtm_Cmu_Clk_3, (float32)hz);
             IfxGtm_Cmu_enableClocks(&MODULE_GTM, IFXGTM_CMU_CLKEN_CLK3);
+            IfxGtm_Atom_Ch_setClockSource(atom, channel, IfxGtm_Cmu_Clk_3);
             break;
         case IfxGtm_Atom_4:
             atomConfig->clock = IfxGtm_Cmu_Clk_4;
             IfxGtm_Cmu_setClkFrequency(&MODULE_GTM, IfxGtm_Cmu_Clk_4, (float32)hz);
             IfxGtm_Cmu_enableClocks(&MODULE_GTM, IFXGTM_CMU_CLKEN_CLK4);
+            IfxGtm_Atom_Ch_setClockSource(atom, channel, IfxGtm_Cmu_Clk_4);
             break;
         case IfxGtm_Atom_5:
             atomConfig->clock = IfxGtm_Cmu_Clk_5;
             IfxGtm_Cmu_setClkFrequency(&MODULE_GTM, IfxGtm_Cmu_Clk_5, (float32)hz);
             IfxGtm_Cmu_enableClocks(&MODULE_GTM, IFXGTM_CMU_CLKEN_CLK5);
+            IfxGtm_Atom_Ch_setClockSource(atom, channel, IfxGtm_Cmu_Clk_5);
             break;
     }
 
-    atomConfig->atom = tch->timHw->triggerOut->atom;
-    atomConfig->timerChannel = tch->timHw->triggerOut->channel;
-    //atomConfig->base.isrPriority = 0; // Set interrupt priority
-    //atomConfig->base.isrProvider = IfxSrc_Tos_cpu0; // Set interrupt provider
-
-    IfxGtm_Atom_Timer_init(tch->timHw->tim, atomConfig); //init ATOM
-    IfxGtm_Atom_Ch_setCompareZero(tch->timHw->tim->atom, tch->timHw->triggerOut->channel, period);
+    IfxGtm_Atom_Ch_setCompareZero(atom, channel, period);
+    IfxGtm_Atom_Ch_setCompareZeroShadow(atom, channel, period);
 }
 
 void impl_enableTimer(TCH_t * tch){
@@ -131,41 +141,125 @@ void impl_timerChCaptureCompareEnable(TCH_t * tch, bool enable){
 }
 
 void impl_timerPWMConfigChannel(TCH_t * tch, uint16_t value){
-    // PWM Part of ATOM TIMER
-    IfxGtm_Atom_Ch_setSignalLevel(tch->timHw->tim->atom, tch->timHw->triggerOut->channel, Ifx_ActiveState_high);
+    Ifx_GTM_ATOM *atom = tch->timHw->tim->atom;
+    IfxGtm_Atom_Ch channel = tch->timHw->tim->timerChannel;
 
-    IfxGtm_Atom_Ch_setMode(tch->timHw->tim->atom, tch->timHw->triggerOut->channel, IfxGtm_Atom_Mode_outputPwm);
-
+    IfxGtm_Atom_Ch_setSignalLevel(atom, channel, Ifx_ActiveState_high);
+    IfxGtm_Atom_Ch_setMode(atom, channel, IfxGtm_Atom_Mode_outputPwm);
     IfxGtm_PinMap_setAtomTout(tch->timHw->triggerOut, IfxPort_OutputMode_pushPull, IfxPort_PadDriver_cmosAutomotiveSpeed1);
-
-    IfxGtm_Atom_Ch_setCompareOne(tch->timHw->tim->atom, tch->timHw->triggerOut->channel, (uint32_t)value);
+    IfxGtm_Atom_Ch_setCompareOneShadow(atom, channel, (uint32_t)value);
+    IfxGtm_Atom_Agc_enableChannelUpdate(tch->timHw->tim->agc, channel, TRUE); //CM0, CM1 and CLK_SRC will be updated from shadow registers when CN0 = CM0
     return;
 }
 
 void impl_timerPWMStart(TCH_t * tch){
-    IfxGtm_Atom_Agc_enableChannel(tch->timHw->tim->agc, tch->timHw->triggerOut->channel, TRUE, FALSE);
-    IfxGtm_Atom_Agc_enableChannelOutput(tch->timHw->tim->agc, tch->timHw->triggerOut->channel, TRUE, FALSE);
+    IfxGtm_Atom_Agc_enableChannel(tch->timHw->tim->agc, tch->timHw->tim->timerChannel, TRUE, FALSE);
+    IfxGtm_Atom_Agc_enableChannelOutput(tch->timHw->tim->agc, tch->timHw->tim->timerChannel, TRUE, FALSE);
     IfxGtm_Atom_Agc_trigger(tch->timHw->tim->agc);
     return;
 }
 
 bool impl_timerPWMConfigChannelDMA(TCH_t * tch, void * dmaBuffer, uint8_t dmaBufferElementSize, uint32_t dmaBufferElementCount){
-    UNUSED(tch);
-    UNUSED(dmaBuffer);
-    UNUSED(dmaBufferElementSize);
     UNUSED(dmaBufferElementCount);
-    return 0;
+
+    if(dmaBuffer == NULL_PTR){
+        return false;
+    }
+
+    if(dmaBufferElementSize != sizeof(timerDMASafeType_t)){
+        return false;
+    }
+
+    tch->dmaBuffer = dmaBuffer;
+
+    timerDef_t timerDef = timerDefinitions[timer2id(tch->timHw->tim)];
+    IfxGtm_Atom_Timer_Config *atomConfig = timerDef.config;
+    volatile Ifx_GTM_ATOM_AGC * agc = tch->timHw->tim->agc;
+    //disable channel
+    IfxGtm_Atom_Agc_enableChannel(agc, tch->timHw->triggerOut->channel, FALSE, FALSE);
+    IfxGtm_Atom_Agc_enableChannelOutput(agc, tch->timHw->triggerOut->channel, FALSE, FALSE);
+    IfxGtm_Atom_Agc_trigger(agc);
+
+    IfxDma_ChannelId channelId = DMA_CHANNEL_TIMER_0 + timer2id(tch->timHw->tim);
+
+    //configure DMA vector
+    atomConfig->base.isrPriority = channelId;
+    atomConfig->base.isrProvider = IfxSrc_Tos_dma;
+    
+    //enable interrupt
+    volatile Ifx_SRC_SRCR *src;
+    //this can cause race conditions when CM1 comes close to CM0. However, in DSHOT this is not the case
+    IfxGtm_Atom_Ch_setNotification(tch->timHw->tim->atom, tch->timHw->tim->timerChannel, IfxGtm_IrqMode_pulseNotify, TRUE, FALSE);
+    src = IfxGtm_Atom_Ch_getSrcPointer(tch->timHw->tim->gtm, atomConfig->atom, tch->timHw->tim->timerChannel);
+    IfxSrc_init(src, IfxSrc_Tos_dma, channelId);
+    IfxSrc_enable(src);
+
+    //now DMA config starts
+    IfxDma_Dma_Config dmaConfig;
+    IfxDma_Dma_initModuleConfig(&dmaConfig, &MODULE_DMA);
+    
+    IfxDma_Dma_initModule(&(tch->dma), &dmaConfig);
+
+    IfxDma_Dma_ChannelConfig * dmaChnCfg = &(tch->dmaChnCfg);
+    IfxDma_Dma_initChannelConfig(dmaChnCfg, &(tch->dma));
+
+    dmaChnCfg->transferCount = 1;
+    dmaChnCfg->requestMode = IfxDma_ChannelRequestMode_completeTransactionPerRequest;
+    dmaChnCfg->moveSize = IfxDma_ChannelMoveSize_32bit; //equals timerDMASafeType_t
+    dmaChnCfg->operationMode = IfxDma_ChannelOperationMode_continuous;
+    dmaChnCfg->hardwareRequestEnabled = TRUE;
+    dmaChnCfg->shadowControl = IfxDma_ChannelShadow_linkedList;
+    dmaChnCfg->channelId = channelId;
+    
+    return true;
 }
+
 void impl_timerPWMPrepareDMA(TCH_t * tch, uint32_t dmaBufferElementCount){
-    UNUSED(tch);
-    UNUSED(dmaBufferElementCount);
+    impl_timerPWMStopDMA(tch);
+
+    //create linked list
+    Ifx_DMA_CH * dmaLinkedList = ((timerDmaSource_t *)tch->dmaBuffer)->dmaLinkedList;
+    uint32_t * dmaSourceBuffer = ((timerDmaSource_t *)tch->dmaBuffer)->dmaBuffer;
+    IfxDma_Dma_ChannelConfig * dmaChnCfg = &(tch->dmaChnCfg);
+
+    for(uint32_t i = 0; i < dmaBufferElementCount; i++){
+        dmaChnCfg->sourceAddress = dmaSourceBuffer+i;
+        dmaChnCfg->destinationAddress = impl_timerCCR(tch);
+
+        //point to next list element, but dont point the last anywhere
+        if(i < (dmaBufferElementCount-1)){
+            dmaChnCfg->shadowAddress = dmaLinkedList+i+1;
+        }
+
+        IfxDma_Dma_initLinkedListEntry((dmaLinkedList+i), dmaChnCfg);
+
+        if(i == 0){
+            IfxDma_Dma_initChannel(&tch->dmaChannel, dmaChnCfg);
+        }
+    }
+
+    tch->dmaState = TCH_DMA_READY;
     return;
 }
 void impl_timerPWMStartDMA(TCH_t * tch){
-    UNUSED(tch);
+    //start timer
+    impl_timerPWMStart(tch);
+
+    tch->dmaState = TCH_DMA_ACTIVE;
     return;
 }
 void impl_timerPWMStopDMA(TCH_t * tch){
-    UNUSED(tch);
+    //terminate if any transaction is in progress
+    IfxDma_disableChannelTransaction(&(tch->dma), tch->dmaChannel.channelId);
+
+    timerDef_t timerDef = timerDefinitions[timer2id(tch->timHw->tim)];
+    IfxGtm_Atom_Timer_Config *atomConfig = timerDef.config;
+    volatile Ifx_GTM_ATOM_AGC * agc = tch->timHw->tim->agc;
+    //disable channel
+    IfxGtm_Atom_Agc_enableChannel(agc, tch->timHw->triggerOut->channel, FALSE, FALSE);
+    IfxGtm_Atom_Agc_enableChannelOutput(agc, tch->timHw->triggerOut->channel, FALSE, FALSE);
+    IfxGtm_Atom_Agc_trigger(agc);
+
+    tch->dmaState = TCH_DMA_IDLE;
     return;
 }
